bf78c05fe92806711b6ce814fbd24e7b9a46d330
dsanchezsantos <dsanchezsantos@gmail.com>
2025-07-23 14:13:29 -0300

    readme

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StyleSheet, View } from 'react-native';
 import GarageScreen from './src/screens/GarageScreen';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <GarageScreen />
+      <StatusBar style='light' />
     </View>
   );
 }
